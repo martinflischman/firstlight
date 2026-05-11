c87d4aadb07b3fc4f9176613e376fb92ee912340
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-11 16:44:18 +0200

    feat: display unsplash photographer name on page
    - add user-name div to index.html positioned bottom-left
    - set textContent to display photographer name from data.user.name

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const userName = document.getElementById("user-name");
+
 async function bgImage() {
   const response = await fetch(
     "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
   );
   const data = await response.json();
 
-  document.body.style.backgroundImage = `url(${data.urls.full})`;
+  document.body.style.backgroundImage = `url(${data.urls.regular})`;
+
+  userName.textContent = `Photo by: ${data.user.name}`;
+
+  console.log(data);
 }
 
 bgImage();
