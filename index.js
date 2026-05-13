097ac53f1549744c14aad84348fcd417a50d2bb4
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-13 16:40:37 +0200

    chore: add manifest.json and icons folder, switch to full image url
    - add manifest.json with manifest_version 3, firstlight name and description
    - add icons folder with icon.png
    - change data.urls.regular to data.urls.full in bgImage fetch

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function bgImage() {
     );
     const data = await response.json();
 
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
     userName.textContent = `Image by: ${data.user.name}`;
   } catch (error) {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1555861496-0666c8981751?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`;
